bb25485fe5346841a1598b70f483b44d0d18e74e
mayankmaheshwar <mayank.maheshwari625@gmail.com>
2023-06-16 23:47:03 +0530

    initial3 commit

--- kvstore/src/main.rs
+++ kvstore/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let mut arguments= std::env::args().skip(1);
     let key = arguments.next().unwrap();
     let value = arguments.next().unwrap();
-    println!("The key is '{}' and value is '{}'", key, value);
+    println!("The key is '{}' and the value is '{}'", key, value);
 
 
     

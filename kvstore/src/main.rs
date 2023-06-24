c5a26adc54dd52f722cef84cc557192b7c1ca428
mayankmaheshwar <mayank.maheshwari625@gmail.com>
2023-06-24 13:56:56 +0530

    just initials

--- kvstore/src/main.rs
+++ kvstore/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     let value1 = arguments.next().unwrap();
     println!("The key is '{}' and the value is '{}'", key1,value1);
     let mut key2 = arguments.next().unwrap();
+    key2 = "dg_key".to_owned();
+
     print!( "The key is '{}' and the value is '{}'", key2, value1);
     
 }
